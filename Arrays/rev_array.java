3a1e0746b4c711db60e13b57499d501024c80c11
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-06-29 00:02:31 +0530

    modified

--- Arrays/rev_array.java
+++ Arrays/rev_array.java
@@ -8,8 +8,7 @@ public class rev_array {
         int i = 0;
         int j = arr.length-1;
         int temp = 0;
-        while (i 
-            <= j) {
+        while (i<= j) {
             temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
