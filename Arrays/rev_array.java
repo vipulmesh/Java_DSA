be407290e3ba31fdc8aa0072cae5da5ed0abb54e
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-06-29 00:01:03 +0530

    array_manupaltion1

---
+++ Arrays/rev_array.java
@@ -0,0 +1,22 @@
+package DSA.Arrays;
+
+import java.util.Arrays;
+
+public class rev_array {
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 6, 8, 9 };
+        int i = 0;
+        int j = arr.length-1;
+        int temp = 0;
+        while (i 
+            <= j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
