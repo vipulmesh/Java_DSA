c541a3632ac008b0c8affe85e5883100da956811
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-06-30 16:30:02 +0530

    two sum hogya

---
+++ Arrays/two_sum.java
@@ -0,0 +1,21 @@
+package DSA.Arrays;
+
+import java.util.Arrays;
+
+public class two_sum {
+    public static void main(String[] args) {
+        int target =7;
+        int[] arr = { 2, 1, 3, 5, 4, 6 };
+        int [] ans = {0,0};
+        // int [] ans = new int[//twosum ke liye 2 likehn ];
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+               if(arr[i]+arr[j]==target){
+                ans = new int[]{arr[i], arr[j]};
+               }
+            }
+        }
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
