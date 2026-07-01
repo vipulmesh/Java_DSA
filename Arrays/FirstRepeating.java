4c6177bb567a5ba007106b173064d4f859eeee87
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-07-01 18:42:53 +0530

    done

---
+++ Arrays/FirstRepeating.java
@@ -0,0 +1,19 @@
+package DSA.Arrays;
+public class FirstRepeating {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 2, 2, 3, 3, 4};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No repeating element");
+    }
+}
