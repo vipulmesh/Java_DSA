c5f79fc7bf9a3c51fbb61ff94891fb7722b7c3ed
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-07-10 17:43:35 +0530

    started bs on 10JULY

---
+++ BInarySearch/binarySearchCode.java
@@ -0,0 +1,28 @@
+package DSA.BInarySearch;
+
+public class binarySearchCode {
+    public static void main(String[] args) {
+
+        int arr[] = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println(mid);
+                return;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+}
