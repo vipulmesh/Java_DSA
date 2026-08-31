18002e78b4df5a594fda7cb741e0a8936e15e9f5
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-08-31 18:44:38 +0530

    done

---
+++ LinkedList/remove_duplicates.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+    if(head==null){
+        return head;
+    }
+    if(head.next == null){
+        return head;
+    }
+    //>1 node wala case
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        while(curr!=null){
+            if(prev.val!=curr.val){
+                prev = prev.next;
+                curr=curr.next;
+            }
+            else{
+                prev.next=curr.next;
+                curr=curr.next;
+            }
+        }
+        return head;
+    }
+}
