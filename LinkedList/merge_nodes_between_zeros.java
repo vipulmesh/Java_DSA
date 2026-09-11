674e44aebab5b9bf773d7e98f5feaae8366455c0
Vipul Meshram <vipulmeshramv20@gmail.com>
2026-09-11 11:08:28 +0530

    new

---
+++ LinkedList/merge_nodes_between_zeros.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class merge_nodes_between_zeros {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode read =head.next;
+        ListNode write = head;
+        while(read!=null){
+
+            int sum =0;
+            //calculate sum ki value till you get the 0 value wali node
+            while(read.val!=0){
+                sum=sum+read.val;
+                read=read.next;
+            }
+            //insert sum ki value ko wrtie wali position pr 
+            write.val =sum;
+            //delte faltu nodes 
+            write.next=read.next;
+            //read and write ko move krdo 1 step 
+            read=read.next;
+            write=write.next;
+
+
+        }
+        return head;
+    }
+}
